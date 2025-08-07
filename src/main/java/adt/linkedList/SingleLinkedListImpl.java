215448548876bef5a30201358d2b287aad19beb9
124110621 BIANKA VITORIA SILVA LIMA <bianka.vitoria.silva.lima@lcc2-53.lcc.ufcg.edu.br>
2025-08-07 07:52:50 -0300

    single

--- src/main/java/adt/linkedList/SingleLinkedListImpl.java
+++ src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -3,39 +3,67 @@ package adt.linkedList;
 public class SingleLinkedListImpl<T> implements LinkedList<T> {
 
 	protected SingleLinkedListNode<T> head;
+	protected SingleLinkedListNode<T> tail;
+	protected int elements;
 
 	public SingleLinkedListImpl() {
 		this.head = new SingleLinkedListNode<T>();
+		this.tail = new SingleLinkedListNode<T>();
+		elements = 0;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		return this.head == null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		return elements;
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> nodeAux = head;
+		SingleLinkedListNode<T> resposta = null;
+		while (nodeAux != null) {
+			if (element instanceof SingleLinkedListNode) {	
+				if (nodeAux.equals(element)) {
+					resposta = nodeAux;
+				}
+			}
+			nodeAux = nodeAux.getNext();
+		}
+		
+		return (T)resposta;
 	}
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> newNode = new SingleLinkedListNode<T>(element, null);
+		if (isEmpty()) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			tail.setNext(newNode);
+			this.tail = newNode;
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> nodeAux = head;
+		SingleLinkedListNode<T> prevAux = null;
+		while (nodeAux != null) {
+			if (element instanceof SingleLinkedListNode) {
+				if (nodeAux.equals(element)) {
+					if (prevAux == null) {
+						
+					}
+					prevAux.setNext(nodeAux.next);
+				}
+			}
+		}
 	}
 
 	@Override
